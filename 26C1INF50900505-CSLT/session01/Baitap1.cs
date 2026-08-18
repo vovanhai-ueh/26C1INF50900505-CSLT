4c1d51880a4b4e2898fac3d2de84d22702419aec
vovanhai <vovanhai@ueh.edu.vn>
2026-08-18 16:08:18 +0700

    init git

--- 26C1INF50900505-CSLT/session01/Baitap1.cs
+++ 26C1INF50900505-CSLT/session01/Baitap1.cs
@@ -6,5 +6,13 @@ namespace _26C1INF50900505_CSLT.session01
 {
     internal class Baitap1
     {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" XIn Chao pakon");
+            Console.WriteLine("Bonjour tout le monde");
+            Console.WriteLine("Ciao mondo");
+            Console.WriteLine(6 + 7);
+        }
     }
 }
--- 26C1INF50900505-CSLT/session01/Program.cs
+++ 26C1INF50900505-CSLT/session01/Program.cs
@@ -1,11 +1,4 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine(" XIn Chao pakon");
-        Console.WriteLine("Bonjour tout le monde");
-        Console.WriteLine("Ciao mondo");
-        Console.WriteLine(6+7);
-    }
+    
 }
