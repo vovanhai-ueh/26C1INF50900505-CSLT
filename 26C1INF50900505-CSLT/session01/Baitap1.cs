c18f83bc7948e4aef1b1117ee6a834dc395fdb8d
UEH <UEH@B507GV>
2026-08-18 16:04:05 +0700

    Add project files.

---
+++ 26C1INF50900505-CSLT/session01/Baitap1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _26C1INF50900505_CSLT.session01
+{
+    internal class Baitap1
+    {
+    }
+}
---
+++ 26C1INF50900505-CSLT/session01/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(" XIn Chao pakon");
+        Console.WriteLine("Bonjour tout le monde");
+        Console.WriteLine("Ciao mondo");
+        Console.WriteLine(6+7);
+    }
+}
